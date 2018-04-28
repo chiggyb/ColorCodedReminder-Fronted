5f63681bd391879908a5e384c6c8ffd2af93b419
Chirag Bhardwaj <chirag.bhardwaj22@gmail.com>
2018-04-28 03:33:26 -0400

    built the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShowModelError } from './shared/showModelError.component';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,10 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 // import { PhoneTextboxComponent } from './controlValueAccessor/phone-textbox.component';
 import { CamelToTitlePipe } from './shared/camel-to-title.pipe';
 import { HttpModule } from '@angular/http';
+import { ShowError } from './shared/showError.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule,HttpModule ],
-  declarations: [ AppComponent, AppRoutingModule.components,CamelToTitlePipe ],
+  declarations: [ AppComponent, AppRoutingModule.components, CamelToTitlePipe],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
